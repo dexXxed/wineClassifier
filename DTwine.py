94a8e3344603dec38fb08c0fa18be0568be5fbac
dexxxed <denis22052000@gmail.com>
2020-01-16 17:35:56 +0200

    Making comments and refactor

--- DTwine.py
+++ DTwine.py
@@ -1,35 +1,37 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn import tree
-import graphviz
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 
+# Считываем данные из CSV файла с помощью функции read_csv
+# В wine_train храниться обучающая выборка
+# В wine_test храниться тестовая выборка
 wine_train = pd.read_csv("data/wine_X_train.csv")
 wine_test = pd.read_csv("data/wine_X_test.csv")
 
+# Удаляем столбец 'quality' в xTrain,
+# yTrain присваиваем значение данного стобца и
+# xTest даем весь датафрэйм тестовой выборки
 xTrain = wine_train.drop('quality', axis=1)
 yTrain = wine_train['quality']
 xTest = wine_test
 
-print(xTrain)
-print(yTrain)
-print(xTest)
-
 clfTre = tree.DecisionTreeClassifier(max_depth=7)
 clfTre.fit(xTrain, yTrain)
-clfTre = clfTre.predict(xTrain)
+clfTre = clfTre.predict(xTest)
 
-print(clfTre)
+print("DT (ID3) classifier results: \n", clfTre)
 
-print ("-------------------------")
+print("-------------------------")
 
 rfc = RandomForestClassifier(n_estimators=200)
 rfc.fit(xTrain, yTrain)
 pred_rfc = rfc.predict(xTest)
+print("Random forest classifier results: \n", pred_rfc)
+
+pred_rfc = np.where(pred_rfc >= 6, pred_rfc, 0)
+pred_rfc = np.where(pred_rfc == 0, pred_rfc, 1)
 print(pred_rfc)
 
 
-
-
+np.savetxt("data/wine_X_result.txt", pred_rfc, newline="\n", delimiter=",", fmt='%d')
